a78868f20eeec467e6f86bf902ff06d5867ee586
曹昕卉 <caoxh@ctrip.com>
2020-05-27 14:52:08 +0800

    删除冗余

--- source/_posts/react.js
+++
@@ -1,6 +0,0 @@
-const state={filter:'completed',todos:['Learn React']}
-const newState={...state,todos:[...state.todos,'Learn Redux']}
-const newState2 = Object.assign({},state,{todos:[
-        ...state.todos,
-        'Learn Redux'
-    ]})
